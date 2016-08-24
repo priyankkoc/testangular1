f5def5f5217c50adf119b7f8b6456954bb0536a6
priyankk-cuelogic <priyank.kochhar@cuelogic.co.in>
2016-08-24 13:36:30 +0530

    changed in deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -114,7 +114,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
 :: 5. Running GULP task
 IF EXIST "%DEPLOYMENT_TARGET%" (
   pushd "%DEPLOYMENT_TARGET%"
-  call .\node_modules\.bin\gulp --env development 
+  call .\node_modules\.bin\gulp  
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
