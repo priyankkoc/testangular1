040f515f1054b18fd5f303528157d97b4fc3944c
priyankkoc <kochhar.priyank@gmail.com>
2016-08-24 15:06:49 +0530

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -117,7 +117,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
 echo running gulp
 IF EXIST "%DEPLOYMENT_TARGET%" (
   pushd "%DEPLOYMENT_TARGET%"
-  call .\node_modules\.bin\gulp --env dev 
+  call .\node_modules\.bin\gulp --env %SITE_FLAVOR% 
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
